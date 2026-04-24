4994772dd0d38599cc97a8593ecd1e793b49f513
Sehi55 <sehi1028@naver.com>
2026-04-25 03:30:23 +0900

    fix: edit package name

--- src/main/java/com/michelet/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/michelet/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.miche_let.eureka_server;
+package com.michelet.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
